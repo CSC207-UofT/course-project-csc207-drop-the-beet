79c972fa298e652a112e41e1a9ba41e20cd53b3f
yy-7105 <leoyang105@gmail.com>
2021-10-14 16:54:56 -0400

    1

--- src/java/Notification.java
+++ src/java/Notification.java
@@ -1,12 +1,3 @@
-import java.util.HashMap;
-
 public class Notification {
-    private boolean scheduleNotificationOn;
-    private boolean toDOListNotificationOn;
-    private HashMap<String, Array<Integer>>;
-
-
-    public Notification() {
-
-    }
 }
+
