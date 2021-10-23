b55fb2be8f2530f52cbbd916e2537aa88d5d7e34
yy-7105 <leoyang105@gmail.com>
2021-10-23 19:48:58 -0400

    Slight change to Main (line 25: change userManager to setting) Setting: implement userLogIn, userLogOff, userChangePassword; take userManager as an attribute (no longer a parameter in each method); login, logoff, register, change password can now all be done in Setting UserManager: add several method to support features in Setting User: new attribute (mySchedule & myToDoList); two new method (changeEmail & changePassword) Tests: UserManagerTest & SettingTest

--- src/java/UserManager.java
+++ src/java/UserManager.java
@@ -8,24 +8,65 @@ public class UserManager {
     private HashMap<String, User> userHashMap;
     private User currentUser;
 
+    /**
+     * Initialize a UserManager with:
+     * - a hashmap whose keys are usernames and values are user objects
+     * - a currentUser = null
+     */
     public UserManager() {
         this.userHashMap = new HashMap<>();
-//        this.currentUser =
+        this.currentUser = null;
     }
 
+    /**
+     *
+     * @param username: username
+     * @return true if username is in the hashmap
+     */
     public boolean containsUser(String username) {
         return this.userHashMap.containsKey(username);
     }
 
-    public void addNewUser(String username, String email, String password) {
-        // TODO: report an exception if username exists
+    /**
+     * Add a new user to the hashmap if username not in hashmap
+     * @param username: username
+     * @param email: email
+     * @param password: password
+     * @return true if successful
+     */
+    public boolean addNewUser(String username, String email, String password) {
+        if (this.containsUser(username)) {
+            return false;
+        }
         User user = new User(username, email, password);
         this.userHashMap.put(username, user);
+        return true;
+    }
+
+    public User getCurrentUser() {
+        return this.currentUser;
+    }
+
+    /**
+     * Change the current user
+     * @param username: username
+     * @return the User object associated with username
+     */
+    public User changeCurrentUser(String username) {
+        this.currentUser = this.getUser(username);
+        return this.currentUser;
+    }
+
+    public void logOffCurrentUser() {
+        this.currentUser = null;
     }
 
     public User getUser(String username) {
-        // TODO: report an exception if username does not exist
-        return this.userHashMap.get(username);
+        return this.userHashMap.get(username);  // return null if username doesn't exist
+    }
+
+    public HashMap<String, User> getUserHashmap() {
+        return this.userHashMap;
     }
 
     /**
@@ -41,7 +82,6 @@ public class UserManager {
             return false;
         }
         // create a new user object and save it to the userHashMap
-        User user = new User(username, email, password);
         this.addNewUser(username, email, password);
         return true;
     }
@@ -62,23 +102,25 @@ public class UserManager {
         return userPassword.equals(inputPassword);  // check if user's password equals input password
     }
 
-//    public boolean userLogOff() {
-//        // Todo
-//    }
-//
 //    public boolean loadUserTasks() {
 //        // Todo
 //    }
-//
-//    public boolean userChangePassword() {
-//        // Todo
-//    }
-//
-//    public boolean userChangeEmail() {
-//        // Todo
-//    }
-//
-//    public static void main(String[] args) {
-//        // Todo
-//    }
+
+    public void currentUserChangePassword(String newPassword) {
+        this.currentUser.changePassword(newPassword);
+    }
+
+    public void currentUserChangeEmail(String newEmail) {
+        this.currentUser.changeEmail(newEmail);
+    }
+
+    public static void main(String[] args) {
+        UserManager um = new UserManager();
+        System.out.println(um.getCurrentUser());
+
+//        HashMap<String, Integer> hm = new HashMap<>();
+//        System.out.println(hm.get("12"));
+//        hm.put("123", 1234);
+//        System.out.println(hm.get("123"));
+    }
 }
