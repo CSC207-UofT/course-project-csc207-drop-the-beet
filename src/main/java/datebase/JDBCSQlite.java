3bd4fc3506b70826dc798bb219533556d37aa79a
Hao Qin <hao.qin@mail.utoronto.ca>
2021-11-10 02:55:06 -0500

    Add SQL

--- src/main/java/datebase/JDBCSQlite.java
+++ src/main/java/datebase/JDBCSQlite.java
@@ -1,12 +1,20 @@
 package datebase;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JDBCSQlite {
     Connection dbConnection;
     Statement stmt;
+
+
+    String createAccountTableSql = "CREATE TABLE ACCOUNT" +
+                                   "(ID INT PRIMARY KEY NOT NULL," +
+                                   " USERNAME TEXT NOT NULL, " +
+                                   " EMAIL    TEXT NOT NULL," +
+                                   " PASSWORD TEXT NOT NULL)";
+
+    String createNewUserSql = "INSERT INTO ACCOUNT VALUES ";
+
+    String getMaxIDSql = "SELECT MAX(ACCOUNT.ID) FROM ACCOUNT";
 
     public void create() {
         try {
@@ -29,14 +37,44 @@ public class JDBCSQlite {
         }
     }
 
-    public static void main(String[] args) {
+    public boolean isTableExist(String table) {
+        boolean flag = false;
+        try {
+            DatabaseMetaData meta = dbConnection.getMetaData();
+            ResultSet rs = meta.getTables(null, null, table, new String[] {"TABLE"});
+            flag = rs.next();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return flag;
+    }
+
+    public void InitizalizeDB() throws SQLException {
+        if (!isTableExist("ACCOUNT")){
+            stmt.executeUpdate(createAccountTableSql);
+        }
+    }
+
+    public void createNewUser(String userName, String userEmail, String userPassword) throws SQLException {
+        ResultSet rs = stmt.executeQuery(getMaxIDSql);
+        rs.next();
+        int nextID = 1;
+        String s = rs.getString("MAX(ACCOUNT.ID)");
+        if (s == null || s.length() <= 0) {
+            stmt.executeUpdate(createNewUserSql + "(" + Integer.toString(nextID) + "," + "'" + userName + "'" + "," + "'" + userEmail + "'" + "," + "'" + userPassword + "'" + ")");
+        } else {
+            nextID = Integer.parseInt(s) + 1;
+            stmt.executeUpdate(createNewUserSql + "(" + Integer.toString(nextID) + "," + "'" + userName + "'" + "," + "'" + userEmail + "'" + "," + "'" + userPassword + "'" + ")");
+        }
+
+    }
+
+    public static void main(String[] args) throws SQLException {
         System.out.println(System.getProperty("user.dir"));
         JDBCSQlite jdbcsqlite = new JDBCSQlite();
         jdbcsqlite.create();
+        jdbcsqlite.InitizalizeDB();
+        jdbcsqlite.createNewUser("UserName", "Test@Email.com", "PAss");
         jdbcsqlite.close();
     }
-
-
-
-
 }
