80e941491756e82a76d6d42f20d13bb9c4ad216d
Zheng Duan <zheng.duan@.mail.utoronto.ca>
2021-11-12 16:36:58 -0500

    sendNotification + getNotification phase1先不implement

--- src/java/SendNotification.java
+++ src/java/SendNotification.java
@@ -1,13 +1,39 @@
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
-public interface SendNotification {
+public class SendNotification {
+    private String name;
+    private boolean changed = false;
+    private List<GetNotification> observers = new ArrayList<>();
 
+    public SendNotification(String name){
+        this.name = name;
+    }
 
-    List<String> content = new ArrayList<>();
+    public void setChanged(){
+        changed = !changed;
+    }
 
-    boolean state = true; //默认开启
+    public void addObserver(GetNotification observer){
+        if(observer.getState() && !observers.contains(observer)) observers.add(observer);
+    }
 
-    public void switchState(); //开关
+    public void deleteObserver(GetNotification observer){
+        if(observer.getState() && observers.contains(observer)) observers.remove(observer);
+    }
+
+    public void sendNotification(String newNotification){
+        for (int i = observers.size()-1; i>=0; i--)
+            observers.get(i).update(this, newNotification);
+    }
+
+    public String toString(){
+        return name;
+    }
+
 
 }
